0490888e1370dfca737bf85a6eb639004e301756
Faraz Shoukat <farazshoukat1@gmail.com>
2026-08-02 23:05:32 +0500

    Add Google Sheets OAuth integration

--- config.py
+++ config.py
@@ -9,3 +9,7 @@ GROQ_API_KEY = os.environ["GROQ_API_KEY"]
 TESSERACT_PATH = os.environ.get("TESSERACT_PATH", "tesseract")
 
 UPLOAD_DIR = "uploads"
+
+GOOGLE_SHEET_ID = os.environ["GOOGLE_SHEET_ID"]
+OAUTH_CLIENT_SECRET_FILE = "credentials/client_secret.json"
+OAUTH_TOKEN_FILE = "credentials/token.json"
